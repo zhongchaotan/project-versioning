1b62582d06f2aadfd0a6fef36e30d224c6f66ec0
zhongchaotan <82837491+zhongchaotan@users.noreply.github.com>
2023-03-27 18:08:11 -0400

    [FRDATA-933]: adding test folder

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-__version__ = "0.0.0"
+__version__ = '0.0.0'
 
 setup(
    name="versioning",
    version=__version__,
+   packages=find_packages(),
 )
