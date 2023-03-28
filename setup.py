f79204a90cdd4586e6c8751621fe9532aaf52677
zhongchaotan <82837491+zhongchaotan@users.noreply.github.com>
2023-03-28 12:18:18 -0400

    Frdata 933 implement project versioning (#1)
    * [FRDATA-933]: initial commit: 1) add dependency file 2) add setup file 3) add project toml file
    
    * [FRDATA-933]: adding test folder
    
    * [FRDATA-933]: update initial version number

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+__version__ = '1.0.0'
+
+setup(
+   name="versioning",
+   version=__version__,
+   packages=find_packages(),
+)
---
+++ test/main.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("test")
+
+if __name__ == "__main__":
+    main()
