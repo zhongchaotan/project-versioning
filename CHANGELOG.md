b4d600fd1779768ba57224bd51c5f35fed7aec6b
zhongchaotan <82837491+zhongchaotan@users.noreply.github.com>
2023-03-31 17:27:52 -0400

    [FRDATA-933] merge in test 2 (#3)
    * feat: commit test 2
    
    * feat: add commitizen
    
    * bump: version 0.0.1 → 0.1.0
    
    * feat: add version_files
    
    * bump: version 0.1.0 → 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,12 @@
-<!--next-version-placeholder-->
+## 0.2.0 (2023-03-31)
+
+### Feat
+
+- add version_files
+
+## 0.1.0 (2023-03-31)
+
+### Feat
+
+- add commitizen
+- commit test 2
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '0.3.0'
+__version__ = '0.2.0'
 
 #####
-# test    1
+# test    2
 setup(
    name="versioning",
    version=__version__,
